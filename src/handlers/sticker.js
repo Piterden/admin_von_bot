095f6aac89826a7270809ab9180684a442ae04c1
Piterden <efremov.a.denis@gmail.com>
2019-07-31 02:02:58 +0300

    Added reply for stickers

--- src/handlers/sticker.js
+++ src/handlers/sticker.js
@@ -1,12 +1,22 @@
 const Markup = require('telegraf/markup')
 
+const { debug } = require('@/helpers')
+
 module.exports = () => async (ctx) => {
   ctx.deleteMessage(ctx.message.message_id)
+
+  const extra = Markup.inlineKeyboard([
+    Markup.callbackButton('Delete', 'action=delete'),
+  ]).extra()
+
+  if (ctx.message.reply_to_message) {
+    extra.reply_to_message_id = ctx.message.reply_to_message.message_id
+    debug(extra)
+  }
+
   ctx.tg.sendSticker(
     ctx.chat.id,
     ctx.message.sticker.file_id,
-    Markup.inlineKeyboard([
-      Markup.callbackButton('Delete', 'action=delete'),
-    ]).extra(),
+    extra,
   )
 }
