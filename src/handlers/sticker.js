0745e60f6e0898b617fe5efb5e090c5b5a94b560
Piterden <efremov.a.denis@gmail.com>
2019-08-01 01:56:52 +0300

    Added timer to stickers

--- src/handlers/sticker.js
+++ src/handlers/sticker.js
@@ -2,10 +2,13 @@ const Markup = require('telegraf/markup')
 
 const { debug } = require('@/helpers')
 
+const DELAY = 90000
+
 module.exports = () => async (ctx) => {
   ctx.deleteMessage(ctx.message.message_id)
 
   const extra = Markup.inlineKeyboard([
+    Markup.callbackButton(`From: ${ctx.from.first_name}`, 'nope'),
     Markup.callbackButton('Delete', 'action=delete'),
   ]).extra()
 
@@ -14,9 +17,13 @@ module.exports = () => async (ctx) => {
     debug(extra)
   }
 
-  ctx.tg.sendSticker(
+  const message = await ctx.tg.sendSticker(
     ctx.chat.id,
     ctx.message.sticker.file_id,
     extra,
   )
+
+  setTimeout(() => {
+    ctx.deleteMessage(message.message_id)
+  }, DELAY)
 }
