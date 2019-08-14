c7fdb7a676e4c491eca1087bf4c8c22f74224c5a
Piterden <efremov.a.denis@gmail.com>
2019-08-14 23:02:25 +0300

    Fixed migrations

---
+++ migrations/20190727051914_create_likes_table.js
@@ -0,0 +1,25 @@
+exports.up = async (knex) => {
+  if (!await knex.schema.hasTable('likes')) {
+    return knex.schema.createTable('likes', (table) => {
+      table.bigInteger('chat_id')
+      table.bigInteger('message_id').unsigned()
+      table.bigInteger('user_id').unsigned()
+      table.integer('likes').unsigned()
+      table.integer('dislikes').unsigned()
+      table.json('message')
+      table.timestamps(['created_at', 'updated_at'])
+
+      table.primary(['chat_id', 'message_id'])
+      table.index('likes')
+      table.index('dislikes')
+    })
+  }
+  return null
+}
+
+exports.down = async (knex) => {
+  if (await knex.schema.hasTable('likes')) {
+    return knex.schema.dropTable('likes')
+  }
+  return null
+}
