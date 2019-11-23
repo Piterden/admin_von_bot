ddba7c9f108746fe73d275e059029150957b2816
Developer <you@example.com>
2019-11-23 11:26:09 +0200

    where clause

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,4 +1,5 @@
 const knex = require('knex')
+const { errorHandler } = require('@/helpers')
 
 module.exports = () => async (ctx) => {
   const chatMember = await ctx.tg.getChatMember(ctx.chat.id, ctx.from.id)
@@ -12,7 +13,12 @@ module.exports = () => async (ctx) => {
     return
   }
 
-  const stats = await ctx.database('actions').select('action', 'button', knex.raw('count(*) as cnt')).groupBy(['action', 'button'])
+  const stats = await ctx.database('actions')
+    .select('action', 'button', knex.raw('count(*) as cnt'))
+    .where({ id: Number(ctx.chat.id) })
+    .groupBy(['action', 'button'])
+    .catch(errorHandler)
+
   const text = stats.map(row => `${row.action} ${row.button ? row.button : ''} ${row.cnt}`).join('\n')
   await ctx.reply(text)
 }
